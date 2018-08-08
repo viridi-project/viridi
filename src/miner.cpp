0845e0d10c7d554ecd71e2368650a78fddf2c0df
Viridi Coin <41009797+viridi-project@users.noreply.github.com>
2018-08-08 14:51:41 +0300

    changes

--- src/miner.cpp
+++ src/miner.cpp
@@ -246,7 +246,7 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
         TxPriorityCompare comparer(fSortedByFee);
         std::make_heap(vecPriority.begin(), vecPriority.end(), comparer);
 
-        vector<CBigNum> vBlockSerials;
+        
         
         while (!vecPriority.empty()) {
             // Take highest priority transaction off the priority queue:
@@ -312,8 +312,7 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
             nBlockSigOps += nTxSigOps;
             nFees += nTxFees;
 
-            for (const CBigNum bnSerial : vTxSerials)
-                vBlockSerials.emplace_back(bnSerial);
+            
 
             if (fPrintPriority) {
                 LogPrintf("priority %.1f fee %s txid %s\n",
