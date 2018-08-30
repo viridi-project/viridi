36499943bebb36db172295f9ef86f12a88be202c
Viridi Coin <41009797+viridi-project@users.noreply.github.com>
2018-08-30 11:33:00 +0300

    initial changfes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -214,7 +214,7 @@ public:
         //convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
         fRequireRPCPassword = true;
-        fMiningRequiresPeers = false;
+        fMiningRequiresPeers = true;
         fDefaultConsistencyChecks = true;
         fRequireStandard = true;
         fMineBlocksOnDemand = false;
